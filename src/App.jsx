d1dd36e75bd9707592572c95b7e8ced41cc8358f
Luciano Moyano <lucianoarielmoyano@outlook.com>
2023-07-12 17:13:09 -0300

    Portfolio-Luciano

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { Header } from "./components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        {/* Aquí puedes agregar el resto de tu contenido */}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white py-4">
+      <h1 className="text-2xl font-bold text-center">Mi Portfolio</h1>
+    </header>
+  );
+};
